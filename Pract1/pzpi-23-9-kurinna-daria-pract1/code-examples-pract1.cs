2d0eb8dca9f8bb694b5d72b71a4630b37fc50b9c
NureKurinnaDaria <163350267+NureKurinnaDaria@users.noreply.github.com>
2026-04-01 10:39:24 +0300

    Implement Strategy Pattern for payment methods

--- Pract1/pzpi-23-9-kurinna-daria-pract1/code-examples-pract1.cs
+++ Pract1/pzpi-23-9-kurinna-daria-pract1/code-examples-pract1.cs
@@ -1,1 +1,51 @@
-// Strategy Pattern example
+using System;
+
+public interface IPaymentStrategy
+{
+    void Pay(int amount);
+}
+
+public class CreditCardPayment : IPaymentStrategy
+{
+    public void Pay(int amount)
+    {
+        Console.WriteLine("Оплата кредитною карткою на суму: " + amount);
+    }
+}
+
+public class PayPalPayment : IPaymentStrategy
+{
+    public void Pay(int amount)
+    {
+        Console.WriteLine("Оплата через систему PayPal на суму: " + amount);
+    }
+}
+
+public class PaymentContext
+{
+    private IPaymentStrategy strategy;
+
+    public void SetStrategy(IPaymentStrategy strategy)
+    {
+        this.strategy = strategy;
+    }
+
+    public void ExecutePayment(int amount)
+    {
+        strategy.Pay(amount);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        PaymentContext context = new PaymentContext();
+
+        context.SetStrategy(new CreditCardPayment());
+        context.ExecutePayment(100);
+
+        context.SetStrategy(new PayPalPayment());
+        context.ExecutePayment(200);
+    }
+}
